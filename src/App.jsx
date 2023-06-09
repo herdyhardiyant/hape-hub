05f2099348d1c339874596797f8ecbe410dece5c
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2023-06-09 14:50:17 +0700

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+const phoneDummy = 'https://cdn.tmobile.com/content/dam/t-mobile/en-p/cell-phones/apple/Apple-iPhone-12/Black/Apple-iPhone-12-Black-thumbnail.png'
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className='flex flex-row items-center justify-between align-middle bg-stone-800 p-4 px-6 h-20 shadow-lg'>
+      <h1 className='text-4xl '>Hape Hub_@</h1>
+      <div className='flex flex-row gap-4'>
+        <p className=''>register</p>
+        <p className=''>login</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className='text-blue-500'>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+     </div>
+    <br />
+     <div className='p-8'>
+
+      <div className='flex flex-col relative justify-between bg-stone-800 w-64 h-64 p-4 rounded-lg shadow-lg '>
+        <img className='object-contain w-full h-2/3 ' src={phoneDummy} alt="Phone dummy" />
+        <div className='absolute bottom-0'>
+          <h3>Phone Name #1113</h3>
+          <p>Price: $1000</p>
+        </div>
+    
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+     </div>
     </>
   )
 }
