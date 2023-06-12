daeb19dca586b5a3226c3e377dab6f6db5098d35
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2023-06-13 00:55:18 +0700

    login/register input useState and fix color

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 
 function LoginForm({isLoginOpen, onClose}) {
     
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
     const onLoginBackdropClick = () => { 
         onClose()
     }
@@ -11,21 +15,29 @@ function LoginForm({isLoginOpen, onClose}) {
         event.preventDefault()
         onClose()
     }
+
+    const onUsernameChange = (event) => { 
+        setUsername(event.target.value)
+    }
+
+    const onPasswordChange = (event) => { 
+        setPassword(event.target.value)
+    }
     
     
     return ( 
             <CenterItemOverlay isShown={isLoginOpen} onBackdropClick={onLoginBackdropClick}>
-              <form className="flex flex-col ">
+              <form className="flex flex-col">
                     <h3 className="text-xl">Login</h3>
                     <br />
                     <label>
                         Username:
-                        <input type="text" />
+                        <input className="text-black" type="text" value={username} onChange={onUsernameChange}/>
                     </label>
                     
                     <label>
                         Password:
-                        <input type='password'/>
+                        <input type='password' value={password} onChange={onPasswordChange} className="text-black"/>
                     </label>
                     <div className='flex flex-row justify-between
                     mt-2'>
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,6 +1,24 @@
+
+import { useState } from "react";
 import CenterItemOverlay from "./CenterItemOverlay"
 
 function RegisterForm({onClose, isRegisterOpen}) {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const onUsernameChange = (event) => { 
+        setUsername(event.target.value)
+    }
+
+    const onPasswordChange = (event) => { 
+        setPassword(event.target.value)
+    }
+
+    const onConfirmPasswordChange = (event) => { 
+        setConfirmPassword(event.target.value)
+    }
+    
     const onBackdropClick = () => { 
         onClose()
     }
@@ -16,16 +34,16 @@ function RegisterForm({onClose, isRegisterOpen}) {
                     <br />
                     <label>
                         Username:
-                        <input type="text" />
+                        <input type="text" value={username} onChange={onUsernameChange} className="text-black" />
                     </label>
                     
                     <label>
                         Password:
-                        <input type='password'/>
+                        <input type='password' value={password} onChange={onPasswordChange} className="text-black"/>
                     </label>
                     <label>
                         Confirm Password:
-                        <input type='password'/>
+                        <input type='password' value={confirmPassword} onChange={onConfirmPasswordChange} className="text-black"/>
                     </label>
                     <div className='flex flex-row justify-between
                     mt-2'>
